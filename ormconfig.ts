4fbd6fc8c1d1c085841337c0dc4e68132f695f9a
jvitormelo <jvitor.ovidio2@hotmail.com>
2021-04-13 09:20:21 -0300

    desespero

--- ormconfig.ts
+++ ormconfig.ts
@@ -1,20 +1,26 @@
 module.exports = {
   type: 'postgres',
-  host: process.env.LOCAL_DATABASE_HOST,
+  host: process.env.DATABASE_HOST,
   port: 5432,
-  username: process.env.LOCAL_DATABASE_USERNAME,
-  password: process.env.LOCAL_DATABASE_PASSWORD,
-  database: process.env.LOCAL_DATABASE,
+  username: process.env.DATABASE_USERNAME,
+  password: process.env.DATABASE_PASSWORD,
+  database: process.env.DATABASE,
   synchronize: true,
+  ssl: true,
+  extra: {
+    ssl: {
+      rejectUnauthorized: false
+    }
+  },
   logging: false,
   entities: [
-    'src/database/typeORM/entity/**/*.ts'
+    'src/database/typeORM/entity/**/*.ts', 'dist/src/database/typeORM/entity/**/*.js'
   ],
   migrations: [
-    'src/database/typeORM/migration/**/*.ts'
+    'src/database/typeORM/entity/**/*.ts', 'dist/src/database/typeORM/entity/**/*.js'
   ],
   subscribers: [
-    'src/database/typeORM/subscriber/**/*.ts'
+    'src/database/typeORM/entity/**/*.ts', 'dist/src/database/typeORM/entity/**/*.js'
   ],
   cli: {
     entitiesDir: 'src/database/typeORM/entity',
--- src/database/typeORM/connection.ts
+++ src/database/typeORM/connection.ts
@@ -1,31 +1,23 @@
 import { createConnection } from 'typeorm'
 
 export const databaseConnection = async () => {
-  return process.env.MODE === 'DEV'
-    ? createConnection({
-        type: 'postgres',
-        host: process.env.LOCAL_DATABASE_HOST,
-        port: 5432,
-        username: process.env.LOCAL_DATABASE_USERNAME,
-        password: process.env.LOCAL_DATABASE_PASSWORD,
-        database: process.env.LOCAL_DATABASE,
-        entities: [
-          'src/database/typeORM/entity/**/*.ts'
-        ],
-        synchronize: true,
-        logging: false
-      })
-    : createConnection({
-      type: 'postgres',
-      host: process.env.DATABASE_HOST,
-      port: 5432,
-      username: process.env.DATABASE_USERNAME,
-      password: process.env.DATABASE_PASSWORD,
-      database: process.env.DATABASE,
-      entities: [
-        'src/database/typeORM/entity/**/*.ts'
-      ],
-      synchronize: true,
-      logging: false
-    })
+  return createConnection({
+    type: 'postgres',
+    host: process.env.DATABASE_HOST,
+    port: 5432,
+    username: process.env.DATABASE_USERNAME,
+    password: process.env.DATABASE_PASSWORD,
+    database: process.env.DATABASE,
+    entities: [
+      'src/database/typeORM/entity/**/*.ts', 'dist/src/database/typeORM/entity/**/*.js'
+    ],
+    synchronize: true,
+    ssl: true,
+    extra: {
+      ssl: {
+        rejectUnauthorized: false
+      }
+    },
+    logging: false
+  })
 }
