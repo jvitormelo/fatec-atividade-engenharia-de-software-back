f35d4541f73f7ffc85f8b2bec5e25ef6e2d04b5b
jvitormelo <jvitor.ovidio2@hotmail.com>
2021-04-13 09:22:50 -0300

    desespero

--- src/database/typeORM/connection.ts
+++ src/database/typeORM/connection.ts
@@ -1,31 +1,22 @@
 import { createConnection } from 'typeorm'
 
 export const databaseConnection = async () => {
-  return process.env.MODE === 'DEV'
-    ? createConnection({
-        type: 'postgres',
-        host: process.env.LOCAL_DATABASE_HOST,
-        port: 5432,
-        username: process.env.LOCAL_DATABASE_USERNAME,
-        password: process.env.LOCAL_DATABASE_PASSWORD,
-        database: process.env.LOCAL_DATABASE,
-        entities: [
-          'src/database/typeORM/entity/**/*.ts'
-        ],
-        synchronize: true,
-        logging: false
-      })
-    : createConnection({
-      type: 'postgres',
-      host: process.env.DATABASE_HOST,
-      port: 5432,
-      username: process.env.DATABASE_USERNAME,
-      password: process.env.DATABASE_PASSWORD,
-      database: process.env.DATABASE,
-      entities: [
-        'src/database/typeORM/entity/**/*.ts'
-      ],
-      synchronize: true,
-      logging: false
-    })
+  return createConnection({
+    type: 'postgres',
+    host: process.env.DATABASE_HOST,
+    port: 5432,
+    username: process.env.DATABASE_USERNAME,
+    password: process.env.DATABASE_PASSWORD,
+    database: process.env.DATABASE,
+    entities: [
+      'src/database/typeORM/entity/**/*.ts'
+    ],
+    ssl: true,
+    extra: {
+      ssl: true,
+      rejectUnauthorized: false
+    },
+    synchronize: true,
+    logging: false
+  })
 }
