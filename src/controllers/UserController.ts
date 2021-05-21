cc11d7b6fac2c337b2a86d856eb5cda756226bd0
jvitormelo <jvitor.ovidio2@hotmail.com>
2021-05-21 08:40:03 -0300

    refactor(Views,Controller): Remoção do prisma e Controllers/View relacionada a produtos(eu usava pro boer)

--- src/controllers/UserController.ts
+++ src/controllers/UserController.ts
@@ -11,7 +11,6 @@ class UserController extends AbstractController {
 
   async find (req:Request, res:Response, next: NextFunction) {
     const { id } = req.params
-    console.log(req.user.id)
     const user = await this.prisma.users.findFirst({ where: { id: Number(id) ? Number(id) : req.user.id } })
     if (!user) throw new ErrorHandler(404, 'Usuário não encontrado')
     res.json(ResponseFactory.createResponse('Usuário encontrado', user || {}))
